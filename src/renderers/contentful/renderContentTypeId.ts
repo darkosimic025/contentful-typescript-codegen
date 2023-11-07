941f5235b3bb62597fd3b2c87c0faefa15429557
Darko Simic <darkosimic025@gmail.com>
2023-11-07 14:08:13 +0100

    feat: changed from I to Contentful

--- src/renderers/contentful/renderContentTypeId.ts
+++ src/renderers/contentful/renderContentTypeId.ts
@@ -1,5 +1,5 @@
 import { upperFirst, camelCase } from "lodash"
 
 export default function renderContentTypeId(contentTypeId: string): string {
-  return "I" + upperFirst(camelCase(contentTypeId))
+  return "Contentful" + upperFirst(camelCase(contentTypeId))
 }
